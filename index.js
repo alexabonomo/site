d3a19fa42ee74422f6c266119e492036338faed9
Alexa Bonomo <me@alexaann.net>
2022-05-15 22:09:05 -0700

    new site layout

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
-$(document).ready(function() {
 
-    $(".slidingDiv").hide();
-    $(".show_hide").show();
-
-    $('.show_hide').click(function() {
-          
-         //$(".slidingDiv").slideToggle();
-         var isvisible = $(this).next('.slidingDiv').is(':visible');
-      
-         if ( isvisible ) {
-           $(this).next('.slidingDiv').hide();
-         } else{
-           $(this).next('.slidingDiv').show(); 
-         }
-    });
-
+$(function () {
+  $('a img').hover( function () {
+      $('a img').attr('src', 'images/gardenlabyrinth-hover.png');
+  },
+  function(){
+    $('a img').attr('src', 'images/gardenlabyrinth.png')
+  });
 });
+
+
+// $(document).ready(function() {
+
+//     $(".slidingDiv").hide();
+//     $(".show_hide").show();
+
+//     $('.show_hide').click(function() {
+          
+//          //$(".slidingDiv").slideToggle();
+//          var isvisible = $(this).next('.slidingDiv').is(':visible');
+      
+//          if ( isvisible ) {
+//            $(this).next('.slidingDiv').hide();
+//          } else{
+//            $(this).next('.slidingDiv').show(); 
+//          }
+//     });
+
+// });
